25a916c5e7a4f39d2bba43dba10101f1f4f8627b
shinhonggyu <zowoz0@naver.com>
2022-03-02 00:35:13 +0900

    디지털 시계

---
+++ projects/디지털 시계/script.js
@@ -0,0 +1,22 @@
+const todayElement = document.getElementById("today");
+const timeElement = document.getElementById("time");
+
+function getTime() {
+  const textNode = new Date();
+  todayElement.textContent = textNode;
+  const year = textNode.getFullYear();
+  const month = textNode.getMonth() + 1;
+  const date = textNode.getDate();
+  const hour = textNode.getHours();
+  const minute = textNode.getMinutes();
+  const second = textNode.getSeconds();
+
+  todayElement.textContent = `
+  ${year}년 
+  ${month > 9 ? month : "0" + month}월 
+  ${date > 9 ? date : "0" + date}일`;
+  timeElement.textContent = `${hour}시 ${minute}분 ${second}초`;
+}
+
+getTime();
+setInterval(getTime, 1000);
